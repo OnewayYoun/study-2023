d6d4f4d642d10ec7e5630a213407d843f00f5a5d
OnewayYoun <onewayy91@gmail.com>
2023-07-22 15:44:16 +0900

    feat: solution without sorting

--- Problem Solving/윤한길/01주차 블랙잭.py
+++ Problem Solving/윤한길/01주차 블랙잭.py
@@ -4,12 +4,10 @@ from itertools import combinations
 if __name__ == '__main__':
     N, M = map(int, sys.stdin.readline().split())
     cards = list(map(int, sys.stdin.readline().split()))
-    possible_answers = []
+    answer = float('-inf')
 
     for comb in combinations(cards, 3):
         total = sum(comb)
         if total <= M:
-            possible_answers.append(total)
-
-    possible_answers.sort()
-    print(possible_answers[-1])
+            answer = max(answer, total)
+    print(answer)
