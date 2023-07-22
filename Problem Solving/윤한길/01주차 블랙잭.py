a4681fedfc2f6fbe728f80bf4601a8514be2860a
OnewayYoun <onewayy91@gmail.com>
2023-07-22 15:41:59 +0900

    feat: solved with combinations

--- Problem Solving/윤한길/01주차 블랙잭.py
+++ Problem Solving/윤한길/01주차 블랙잭.py
@@ -1,2 +1,15 @@
+import sys
+from itertools import combinations
+
 if __name__ == '__main__':
-    
+    N, M = map(int, sys.stdin.readline().split())
+    cards = list(map(int, sys.stdin.readline().split()))
+    possible_answers = []
+
+    for comb in combinations(cards, 3):
+        total = sum(comb)
+        if total <= M:
+            possible_answers.append(total)
+
+    possible_answers.sort()
+    print(possible_answers[-1])
