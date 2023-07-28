f98c19f7cf2c2fcd9a376788f35a79ecf3730b5a
OnewayYoun <onewayy91@gmail.com>
2023-07-28 18:58:57 +0900

    feat: added explanation

--- Problem Solving/윤한길/02주차 - 피아노 체조.py
+++ Problem Solving/윤한길/02주차 - 피아노 체조.py
@@ -2,7 +2,6 @@ import sys
 
 input = sys.stdin.readline
 
-
 if __name__ == '__main__':
     N = int(input())
     difficulties = list(map(int, input().split()))
@@ -11,10 +10,15 @@ if __name__ == '__main__':
 
     total = 0
     prefix_sum = [0]
-    for i in range(N-1):
-        if difficulties[i] > difficulties[i+1]:
+    for i in range(N - 1):
+        if difficulties[i] > difficulties[i + 1]:
             total += 1
         prefix_sum.append(total)
     prefix_sum.append(total)
     for question in questions:
+        '''
+        마지막으로 연주하는 y번 악보에선 절대 실수하지 않는다.
+        그러므로 해당 y인덱스 이전까지(question[1] - 1)의 누적합에서 빼준다.
+        '''
+
         print(prefix_sum[question[1] - 1] - prefix_sum[question[0] - 1])
