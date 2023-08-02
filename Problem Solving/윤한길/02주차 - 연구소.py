192e3c794ce31a16f2e7e6409fce3f5b8e8fc18c
OnewayYoun <onewayy91@gmail.com>
2023-08-02 19:50:37 +0900

    feat: solved a problem with dfs

--- Problem Solving/윤한길/02주차 - 연구소.py
+++ Problem Solving/윤한길/02주차 - 연구소.py
@@ -3,8 +3,15 @@ from itertools import combinations
 from copy import deepcopy
 
 
-def dfs():
-    pass
+def dfs(x, y):
+    if not 0 <= x < M or not 0 <= y < N:
+        return
+    if new_graph[y][x] == 0:
+        new_graph[y][x] = 2
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
 
 
 def count_safe_area(graph):
@@ -14,14 +21,16 @@ def count_safe_area(graph):
 if __name__ == '__main__':
     N, M = map(int, sys.stdin.readline().split())
     graph = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-    virus_coordinates = [(i, j) for i in range(M) for j in range(N) if graph[j][i] == 0]
-    empty_coordinates = [(i, j) for i in range(M) for j in range(N) if graph[j][i] == 2]
+    empty_coordinates = [(i, j) for i in range(M) for j in range(N) if graph[j][i] == 0]
+    virus_coordinates = [(i, j) for i in range(M) for j in range(N) if graph[j][i] == 2]
 
     max_sum = float('-inf')
-    for comb in combinations(virus_coordinates, 3):
-        visited = [[False] * M for _ in range(N)]
+    for comb in combinations(empty_coordinates, 3):
         new_graph = deepcopy(graph)
-        dfs()
+        for x, y in comb:
+            new_graph[y][x] = 1
+        for virus_coord in virus_coordinates:
+            new_graph[virus_coord[1]][virus_coord[0]] = 0
+            dfs(virus_coord[0], virus_coord[1])
         max_sum = max(max_sum, count_safe_area(new_graph))
     print(max_sum)
-    
