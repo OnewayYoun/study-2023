745d48fd3607c80bb5b8031f2ec25360ca6db9d5
OnewayYoun <onewayy91@gmail.com>
2023-08-04 02:03:00 +0900

    feat: solved a problem using min_heap

---
+++ Problem Solving/윤한길/03주차 - 최소 힙.py
@@ -0,0 +1,20 @@
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    N = int(input())
+    heap = []
+    answer = []
+    for _ in range(N):
+        x = int(input())
+        if x == 0:
+            if not heap:
+                answer.append(0)
+            else:
+                answer.append(heappop(heap))
+        else:
+            heappush(heap, x)
+
+    print(*answer, sep='\n')
