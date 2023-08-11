032e53e0db2936f4dbac702ce645d882cab4379c
OnewayYoun <onewayy91@gmail.com>
2023-08-11 11:24:23 +0900

    solved with bfs

--- Problem Solving/윤한길/04주차 - 유기농 배추.py
+++ Problem Solving/윤한길/04주차 - 유기농 배추.py
@@ -1,10 +1,11 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10 ** 5)
 
 
-def dfs(x, y) -> bool:
+def dfs(x: int, y: int) -> bool:
     if not 0 <= x < M or not 0 <= y < N:
         return False
     if graph[y][x] == 1:
@@ -17,6 +18,20 @@ def dfs(x, y) -> bool:
     return False
 
 
+def bfs(x: int, y: int):
+    dq = deque([(x, y)])
+    graph[y][x] = 0
+    while dq:
+        x, y = dq.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if not 0 <= nx < M or not 0 <= ny < N:
+                continue
+            if graph[ny][nx] == 1:
+                dq.append((nx, ny))
+                graph[ny][nx] = 0
+
+
 if __name__ == '__main__':
     T = int(input())
     for _ in range(T):
@@ -26,9 +41,14 @@ if __name__ == '__main__':
             X, Y = map(int, input().split())
             graph[Y][X] = 1
 
+        dx = [0, 0, -1, 1]
+        dy = [-1, 1, 0, 0]
         cnt = 0
         for i in range(M):
             for j in range(N):
-                if dfs(i, j):
+                # if dfs(i, j):
+                #     cnt += 1
+                if graph[j][i]:
+                    bfs(i, j)
                     cnt += 1
         print(cnt)
