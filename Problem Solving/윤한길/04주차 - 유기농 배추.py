b471e605af872b8c19315e8f11126edf4ba24eaf
OnewayYoun <onewayy91@gmail.com>
2023-08-11 10:19:57 +0900

    solved with dfs

---
+++ Problem Solving/윤한길/04주차 - 유기농 배추.py
@@ -0,0 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+
+def dfs(x, y) -> bool:
+    if 0 > x or x >= M or 0 > y or y >= N:
+        return False
+    if graph[y][x] == 1:
+        graph[y][x] = 0
+        dfs(x, y + 1)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x - 1, y)
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    T = int(input())
+    for _ in range(T):
+        M, N, K = map(int, input().split())
+        graph = [[0] * M for _ in range(N)]
+        for _ in range(K):
+            X, Y = map(int, input().split())
+            graph[Y][X] = 1
+
+        cnt = 0
+        for i in range(M):
+            for j in range(N):
+                if dfs(i, j):
+                    cnt += 1
+        print(cnt)
