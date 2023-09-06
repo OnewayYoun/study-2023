dba6aa195ea41652cb0836f577a0b52e8d801b3c
OnewayYoun <onewayy91@gmail.com>
2023-09-06 16:55:25 +0900

    solved a problem using deque

---
+++ Problem Solving/윤한길/07주차 - 택배상자.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def solution(order):
+    answer = 0
+    main, sup = deque(range(1, len(order) + 1)), deque()
+    for i in order:
+        if not main and sup[-1] != i:
+            break
+        if sup and sup[-1] == i:
+            sup.pop()
+            answer += 1
+        else:
+            while main:
+                if main[0] == i:
+                    main.popleft()
+                    answer += 1
+                    break
+                sup.append(main.popleft())
+    return answer
+
+
+if __name__ == '__main__':
+    orders = [[4, 3, 1, 2, 5], [5, 4, 3, 2, 1], [3, 5, 4, 2, 1]]
+    for order in orders:
+        print(solution(order))
+    # result = 2, 5
+
+    # order: [3, 5, 4, 2, 1], answer:5
